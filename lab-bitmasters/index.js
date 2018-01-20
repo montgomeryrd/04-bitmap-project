f12819746984d9c96f0684c4671c30a06000f7e9
Jeremy <jpjazzy@hotmail.com>
2018-01-19 17:29:19 -0800

    MADE THAT SHIT LOOK GOOD

--- lab-bitmasters/index.js
+++ lab-bitmasters/index.js
@@ -5,6 +5,42 @@ const Bmp = require('./lib/bitmap');
 
 const retrieve = module.exports = {};
 
-fs.readFile('./test/asset/bitmap.bmp', (err, data) => {
-    let bmp = new Bmp(data);
-});
+let type = process.argv[2];
+
+retrieve.reverse = function() {
+    fs.readFile('./__test__/asset/bitmap.bmp', (err, data) => {
+        let bmp = new Bmp(data);
+
+        
+        if (type === "reverse") { //flip horizontal and vertical
+            bmp.pixelArray.reverse();
+        } else if (type === 'fliph') {
+            let pixelArrayRows = [];
+            for (let i = 0; i < 9900; i+=100){
+                pixelArrayRows.push(bmp.pixelArray.slice(0+i, 99+i).reverse())
+            }
+            bmp.pixelArray = pixelArrayRows.join('');
+        } else if (type === 'moreBlue') {
+            for (let i = 0; i < bmp.colorTable.length/4; i++) {
+                bmp.colorTable[i*4+4] = 255;
+            }
+        } else if (type === 'moreRed') {
+            for (let i = 0; i < bmp.colorTable.length/4; i++) {
+                bmp.colorTable[i*4+2] = 255;
+            }
+        } else {
+            //no transform
+        }
+        
+
+        fs.writeFile('./__test__/asset/bitmaprewrite.bmp', bmp.allData, (err) => {
+            if (err) console.error(err)
+
+            console.log('Image saved')
+        })
+        // console.log(bmp.pixelArray);
+        // console.log(bmp.colorTable);
+    });
+}
+
+retrieve.reverse();
